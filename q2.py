3b991907a9655642efc63d6bb9b1a08aec08b249
kyubin-l <lee.kyubin2176@gmail.com>
2022-11-14 17:25:32 +0000

    question 2 and solution template

---
+++ q2.py
@@ -0,0 +1,70 @@
+from utils.base_solution import BaseSolution
+
+Q_NUM = 2
+
+class Solution(BaseSolution):
+    '''
+    down x INCREASES depth
+    up x DECREASES depth
+    '''
+    def __init__(self, q_num):
+        super().__init__(q_num)
+        self.commands = []
+        self.depth = 0
+        self.hor = 0
+        self.aim = 0
+
+    def load(self):
+        with open(self.filename, 'r') as f:
+            for line in f.readlines():
+                command, value = line.rstrip().split()
+                self.commands.append((command, int(value)))
+
+    def solve(self):
+        self.depth = 0
+        self.hor = 0
+        for command in self.commands:
+            self.execute_command(command)
+        return self.depth*self.hor
+
+    def solve_part_two(self):
+        self.depth = 0
+        self.hor = 0
+        for command in self.commands:
+            self.execute_command_2(command)
+        return self.depth*self.hor
+
+    def execute_command(self, command):
+        '''
+        Receives command as a tuple (command: str, val: int)
+        '''
+        com, val = command[0], command[1]
+        if com == 'forward':
+            self.hor += val
+        elif com == 'down':
+            self.depth += val
+        elif com == 'up':
+            self.depth -= val
+        else:
+            print('Invalid command')
+
+    def execute_command_2(self, command):
+        '''
+        Receives command as a tuple (command: str, val: int)
+        '''
+        com, val = command[0], command[1]
+        if com == 'forward':
+            self.hor += val
+            self.depth += self.aim*val
+        elif com == 'down':
+            self.aim += val
+        elif com == 'up':
+            self.aim -= val
+        else:
+            print('Invalid command')
+
+
+sol = Solution(Q_NUM)
+sol.load()
+print(sol.solve())
+print(sol.solve_part_two())
---
+++ solution_template.py
@@ -0,0 +1,17 @@
+from utils.base_solution import BaseSolution
+
+Q_NUM = 2 
+
+class Solution(BaseSolution):
+    def __init__(self, q_num):
+        super().__init__(q_num)
+
+
+    def load(self):
+        return super().load()
+
+    
+    def solve(self):
+        return super().solve()
+
+sol = Solution(Q_NUM)
