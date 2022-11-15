3e3e0533df96c016a219a9f646472984eca2d12f
kyubin-l <lee.kyubin2176@gmail.com>
2022-11-15 14:07:31 +0000

    q3 part 2

--- 2021_q3.py
+++ 2021_q3.py
@@ -1,18 +1,8 @@
 from utils.base_solution import BaseSolution
+import math
 
 Q_NUM = 3
 YEAR = 2021
-
-
-def bin_to_dec(bin):
-    '''
-    converts binary to decimal, bin: str
-    '''
-    dec = 0
-    n = len(bin)
-    for i in range(n):
-        dec += int(bin[i])*2**(n-i-1)
-    return dec
 
 
 class Solution(BaseSolution):
@@ -31,17 +21,60 @@ class Solution(BaseSolution):
         digit_sum = []
         for digit in range(len(self.nums[0])):
             digit_sum.append(sum(num[digit] for num in self.nums))
-        gamma_list = ['1' if val > len(self.nums)//2 else '0' for val in digit_sum]
-        epsilon_list = ['1' if val <= len(self.nums)//2 else '0' for val in digit_sum]
+        threshold = len(self.nums)//2
+        self.gamma_list = ['1' if val >= threshold else '0' for val in digit_sum]
+        self.epsilon_list = ['1' if val < threshold else '0' for val in digit_sum]
 
-        gamma = ''.join(gamma_list)
-        epsilon = ''.join(epsilon_list)
+        gamma = ''.join(self.gamma_list)
+        epsilon = ''.join(self.epsilon_list)
 
-        power_consumption = bin_to_dec(gamma) * bin_to_dec(epsilon)
+        power_consumption = int(gamma, 2) * int(epsilon, 2)
         return gamma, epsilon, power_consumption
+
+    def solve_part_two(self):
+        """
+        Need to calculate new threshold every digit.
+        For ox:
+        - Use more common digit, if equal, use 1
+        For co:
+        - Use less common digit, if equal, use 0
+        """
+        ox_rating = self.nums.copy()
+        co_rating = self.nums.copy()
+
+        for i in range(len(self.nums[0])):
+            threshold = math.ceil(len(ox_rating)/2)
+            num_ones = sum(rating[i] for rating in ox_rating)
+            ox_val = 1 if num_ones >= threshold else 0
+
+            ox_rating = [value for value in ox_rating if value[i] == ox_val]
+
+            if len(ox_rating) == 1:
+                break
+                    
+        for i in range(len(self.nums[0])):
+            """
+            Possible that all the numbers have same value in a digit: 
+            would remove all numbers from list
+            """
+            threshold = math.ceil(len(co_rating)/2)
+            num_ones = sum(rating[i] for rating in co_rating)
+            co_val = 1 if num_ones < threshold else 0
+
+            co_rating = [value for value in co_rating if value[i] == co_val]
+
+            if len(co_rating) == 1:
+                break
+
+        ox_rating = ''.join(map(str, ox_rating[0]))
+        co_rating = ''.join(map(str, co_rating[0]))
+        life_support_rating = int(ox_rating, 2) * int(co_rating, 2)
+        
+        return ox_rating, co_rating, life_support_rating
 
 
 if __name__ == '__main__':
     sol = Solution(Q_NUM, YEAR)
     sol.load()
     print(sol.solve())
+    print(sol.solve_part_two())
