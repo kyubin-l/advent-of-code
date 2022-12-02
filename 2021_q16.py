9fde362cf54eafa54c70606c89b7419862e99655
kyubin-l <lee.kyubin2176@gmail.com>
2022-12-02 11:28:55 +0000

    blank 15

---
+++ 2021_q16.py
@@ -0,0 +1,21 @@
+from utils.base_solution import BaseSolution
+
+Q_NUM = 16
+YEAR = 2021
+
+class Solution(BaseSolution):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def load(self):
+        return super().load()
+
+    def solve_part_one(self):
+        pass
+
+    def solve_part_two(self):
+        pass
+    
+
+if __name__ == '__main__':
+    sol = Solution(Q_NUM, YEAR)
