b1fb8be8863529961aeb5705d27f4eaa78c8db64
kyubin-l <lee.kyubin2176@gmail.com>
2022-12-11 00:35:37 +0000

    day 10

--- 2022_q10.py
+++ 2022_q10.py
@@ -22,19 +22,52 @@ class Solution(BaseSolution):
                     self.tasks.append(0)
 
     def solve_part_one(self):
+        """
+        if noop i.e. 0, cycle value is previous calue
+        else:
+            cycle value is previous value, and the next value is previous value
+            plus the new value
+        """
         tasks = copy.deepcopy(self.tasks)
-        print(len(tasks))
-        res = [1, 1] # starting 2 values are always 1, 1
-        for i in range(len(tasks)):
-            res.append(sum(tasks[:i+1]))
-
-        return len(res)
-
-        return sum([res[20], res[60], res[100], res[140], res[180], res[220]])
+        x = []  # Keeping values during cycle
+        X = 1
+                
+                
+        for val in tasks:
+            # Keeping values in x during that cycle, index 0 -> 1st cycle
+            x.append((X, X))
+            # If val == 0, it's a noop, so just continue
+            if val:
+                x.append((X, X + val))
+                X += val
+                
+        self.x = x
+        print(x)
+        return sum(x[i][0] * (i + 1) for i in [19, 59, 99, 139, 179, 219])
+                
 
     def solve_part_two(self):
-        pass
-    
+        tasks = copy.deepcopy(self.tasks)
+        
+        CRT = []
+        row = []
+        x = self.x # x holds values at end of cycle, not during
+
+        for i in range(len(x)):
+            X = x[i][0]
+            if len(row) == 40:
+                CRT.append(row)
+                row = []
+
+            if len(row) in [X-1, X, X+1]:
+                row.append('#')
+            else:
+                row.append('.')
+                
+        CRT.append(row)
+
+        for row in CRT:
+            print(row)
 
 if __name__ == '__main__':
     sol = Solution(Q_NUM, YEAR)
