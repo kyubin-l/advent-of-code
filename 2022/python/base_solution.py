5ad72cbe983b23e34c5d6b41972492fff6d8fdbe
Kyubin Lee <lee.kyubin2176@gmail.com>
2023-08-13 10:27:37 +0100

    Fix

--- 2022/python/base_solution.py
+++ 2022/python/base_solution.py
@@ -6,7 +6,7 @@ DIR = Path(__file__).parent
 class BaseSolution(ABC):
     def __init__(self, q_num, year):
         self.q_num = q_num
-        self.filename = DIR / 'inputs' / f'{year}_q{q_num}.txt'
+        self.filename = DIR.parent / 'inputs' / f'{year}_q{q_num}.txt'
 
     @abstractmethod
     def load(self):
